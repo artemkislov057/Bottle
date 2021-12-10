52a1859cf81d645b669bcda8ce66f257978dbedd
predeinnikita <54530655+Nikita4513@users.noreply.github.com>
2021-12-10 17:02:12 +0500

    added-modal-validation

--- frontend/main-page/untitled2/index.js
+++ frontend/main-page/untitled2/index.js
@@ -1,59 +1,83 @@
-let email;
-let password;
-let secondPassword;
+const email = document.getElementById('reg-email');
+const password = document.getElementById('reg-password');
+const secondPassword = document.getElementById('reg-password-two');
+const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-const registerUserButton = document.getElementById('reg-submit');
-registerUserButton.addEventListener('click', () => {
-    email = document.getElementById('reg-email');
-    password = document.getElementById('reg-password');
-    secondPassword = document.getElementById('reg-password-two');
-    // while (password.value !== secondPassword.value) {
-    //     alert('Пароли не совпадают')
-    // }
-    console.log(email.value);
-    console.log(password.value);
-    console.log(secondPassword.value);
-    
-});
+(function registerModal() {
+    document.querySelector('.button-register').addEventListener('click', () => {
+        document.querySelector('.validation-reg').textContent = '';
+    });
 
+    document.getElementById('reg-submit').addEventListener('click', () => {
+        const emailIsValid = emailRegex.test(email.value);
+        if (password.value !== secondPassword.value 
+            | !emailIsValid | password.value === '' 
+            | secondPassword.value === '') {
+            setTimeout(() => {
+                document.querySelector('.hystmodal__close').click();
+                document.querySelector('.button-register').click();
+                checkRegisterData(password.value, secondPassword.value, email.value);
+            }, 0);
+        }
+    });
 
-let gender;
-
-document.querySelector('.gender-man').addEventListener('click', () => {
-    gender = 'male';
-    console.log(gender);
-});
-document.querySelector('.gender-women').addEventListener('click', () => {
-    gender = 'female';
-    console.log(gender);
-});
-
-const createProfileButton = document.getElementById('nickname');
-
-document.getElementById('create-prof-submit').addEventListener('click', () => {
-    const request = {
-        nickname: nickname.value,
-        password: password.value,
-        email: email.value,
-        sex: gender,
-        commercialData: null
+    function checkRegisterData(pass, secondPass, login) {
+        const emailIsValid = emailRegex.test(login);
+        let result = 'Некорректные данные';
+        if (pass== '' && secondPass === '')
+            result = 'Напишите пароль';
+        if (pass != secondPass && emailIsValid) 
+            result = 'Пароли не совпадают';
+        if (!emailIsValid && pass === secondPass) 
+            result = 'Некорректный email';
+        document.querySelector('.validation-reg').textContent = result;
     }
+})();
 
-    console.log('click')
-    fetch('https://localhost:44358/api/account', {
-        method: 'POST',        
-        body: JSON.stringify(request),
-        credentials: 'include',
-        headers: {
-            'Content-Type': 'application/json'
-          }
-    })
-    .then(res => res.json())
-    .then(res => {
-        console.log(res)
-    })
 
-});
+
+
+
+(function profileModal() {
+    let gender;
+    document.querySelector('.gender-man').addEventListener('click', () => gender = 'male');
+    document.querySelector('.gender-women').addEventListener('click', () => gender = 'female');
+    document.getElementById('reg-submit').addEventListener('click', () => {
+        document.querySelector('.validation-prof').textContent = '';
+    });
+
+    document.getElementById('create-prof-submit').addEventListener('click', () => {
+        const request = {
+            nickname: nickname.value,
+            password: password.value,
+            email: email.value,
+            sex: gender,
+            commercialData: null
+        }
+
+        console.log('click');
+        fetch('https://localhost:44358/api/account', {
+            method: 'POST',        
+            body: JSON.stringify(request),
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => {
+            if(!res.ok) {
+                res.text().then(text => {
+                    document.querySelector('.validation-prof').textContent = text;
+                })
+            }
+            else {
+                document.querySelector('.hystmodal__close').click();
+                return res.json();
+            }    
+        })
+        .then(res => console.log(res));
+    });
+})();
 
 document.getElementById('entr-submit').addEventListener('click', () => {
     console.log('click');
