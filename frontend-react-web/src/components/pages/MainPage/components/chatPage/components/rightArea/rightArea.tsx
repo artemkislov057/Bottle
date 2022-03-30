01acd561d5cc599ef9d80c3e5ffd363daade45ff
dimasikBurdin <81648493+dimasikBurdin@users.noreply.github.com>
2022-03-30 22:50:43 +0500

    тестовая попытка в адаптивность чатов

--- frontend-react-web/src/components/pages/MainPage/components/chatPage/components/rightArea/rightArea.tsx
+++ frontend-react-web/src/components/pages/MainPage/components/chatPage/components/rightArea/rightArea.tsx
@@ -5,7 +5,7 @@ import { MessageArea } from "./messageArea";
 
 import defaultAvatar from '../../../interfaceMainPage/components/rightBarProfile/defaultAvatar.svg';
 
-export const MessageAreaChat:React.FC = React.memo(() => {
+export const MessageAreaChat:React.FC = React.memo(() => {    
     return <div className="chat-page-right-side">
         <HeaderRightArea name="Пользователь" rating={3.5} urlAvatar={defaultAvatar} />
         <MessageArea />        
