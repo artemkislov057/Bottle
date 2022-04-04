7cc97632dabc53aa7b93dea034fd40a41b60ea54
dimasikBurdin <81648493+dimasikBurdin@users.noreply.github.com>
2022-04-04 16:59:24 +0500

    fix инпутов при создании бутылки

--- frontend-react-web/src/components/pages/MainPage/components/interfaceMainPage/components/rightBar/mapSettingContainer.tsx
+++ frontend-react-web/src/components/pages/MainPage/components/interfaceMainPage/components/rightBar/mapSettingContainer.tsx
@@ -5,14 +5,14 @@ export const SettingContainer:React.FC = React.memo(() => {
         <div className="right-bar-map-setting-timeLife">
             <label className="right-bar-map-setting-timeLife-title">Длительность записки</label>
             <div className="right-bar-map-setting-TL">
-                <input className="right-bar-map-setting-TL-hour-input" type={"number"} min="1" max='11' value={1} />
+                <input className="right-bar-map-setting-TL-hour-input" type={"number"} min="1" max='11' defaultValue={1} />
                 :
-                <input className="right-bar-map-setting-TL-minut-input" type={"number"} min="0" max='59' value={0}/>
+                <input className="right-bar-map-setting-TL-minut-input" type={"number"} min="0" max='59' defaultValue={0}/>
             </div>                    
         </div>
         <div className="right-bar-map-setting-pick-count">
             <label className="right-bar-map-setting-pick-count-title" htmlFor="pick-count-input">Количество поднятий</label>
-            <input className="right-bar-map-setting-pick-count-input" id="pick-count-input" type={"number"} min='1' value={1}/>
+            <input className="right-bar-map-setting-pick-count-input" id="pick-count-input" type={"number"} min='1' defaultValue={1}/>
         </div>
     </div>
 })
