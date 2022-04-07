75440c4b489ecbd10e6d3c773825a46e78c0edef
dimasikBurdin <81648493+dimasikBurdin@users.noreply.github.com>
2022-04-07 16:33:13 +0500

    добавил обработку времени в popup, отредактировал контейнер описания там же

--- frontend-react-web/src/components/pages/MainPage/components/interfaceMainPage/components/rightBarDescriptBottle/rightBarDescriptBody.tsx
+++ frontend-react-web/src/components/pages/MainPage/components/interfaceMainPage/components/rightBarDescriptBottle/rightBarDescriptBody.tsx
@@ -14,11 +14,31 @@ type TProps = {
 }
 
 export const RightBarDescrBody:React.FC<TProps> = React.memo((props) => {
+    let addres = props.address.split(',').slice(0,2).toString();
+    let time = convertTime(props.timeLife);
+
+    function convertTime(timeInSeconds : number) {
+        console.log(timeInSeconds, 'all seconds');
+        let minutes = (timeInSeconds / 60).toFixed(0);
+        console.log(minutes, 'minutes');
+        let hours = '';
+        if(+minutes > 59) {
+            hours = Math.floor(+minutes / 60).toFixed(0);
+            console.log(hours, 'hours');
+            minutes = (+minutes % 60).toFixed(0);
+            console.log(minutes, 'minutes');
+        }
+        return +hours === 0 ? `${minutes} мин`
+            : +minutes === 0 ? `${hours} ч 0 мин`
+            : `${hours} ч ${minutes} мин`;
+    }
+
+
     return <div className="right-bar-map-popup-body">
         <BodyTitle icon={categoryIcon} titleName={props.titleName}/>        
         <div className="right-bar-map-popup-body-info">
-            <PopupBodyInfo className="addres" title="Адрес:" value={props.address}/>
-            <PopupBodyInfo className="timeLeft" title="До конца мероприятия:" value={props.timeLife}/>
+            <PopupBodyInfo className="addres" title="Адрес:" value={addres}/>
+            <PopupBodyInfo className="timeLeft" title="До конца мероприятия:" value={time}/>
             <PopupBodyInfo className="pickCount" title="Осталось мест:" value={props.countPick} />            
         </div>
         <BodyDescription description={props.description} content={props.content} />        
