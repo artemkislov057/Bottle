9445999a643468057914728ec3e18fb2fcfad97d
dimasikBurdin <81648493+dimasikBurdin@users.noreply.github.com>
2022-04-21 20:19:20 +0500

    добавлено отображение рейтинга звездами(которые соответствуют рейтингу) в разделе своего профиля

--- frontend-react-web/src/components/pages/MainPage/components/interfaceMainPage/components/rightBarProfile/profileRating.tsx
+++ frontend-react-web/src/components/pages/MainPage/components/interfaceMainPage/components/rightBarProfile/profileRating.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { UserRating } from "../leftBar/userRating";
 import { RatingStar } from "./ratingStart";
 
@@ -7,14 +7,43 @@ type TProps = {
 }
 
 export const ProfileRatingContainer:React.FC<TProps> = React.memo((props) => {
+    const [rateStars, setRateStars] = useState(Array<string>());
+    useEffect(() => {
+        if(!props.rating) return
+        let res = []
+
+        if(Number.isInteger(props.rating)) {
+            res = getStars(props.rating, false);            
+        } else {
+            res = getStars(Math.floor(props.rating), true);            
+        }
+        
+        setRateStars(res);
+    }, [props.rating])
+
+    function getStars(intRate: number, haveHalf: boolean) {
+        let res = Array<string>();
+        for(let i = 0; i < intRate; i++) {
+            res.push('');
+        }
+        let emptyCount = 5 - intRate;
+        if(haveHalf) {
+            res.push('half');
+            emptyCount--;
+        }
+        for(let i = 0; i < emptyCount; i++) {
+            res.push('empty');
+        }
+
+        return res;
+    }
+
     return <div className="profile-map-body-rating">
         <UserRating rating={props.rating} />
-        <div className="profile-map-body-rating-stars">	
-            <RatingStar addClass=""/>
-            <RatingStar addClass=""/>
-            <RatingStar addClass=""/>
-            <RatingStar addClass=""/>
-            <RatingStar addClass="half"/>
+        <div className="profile-map-body-rating-stars">
+            {rateStars.map((x, i) => 
+                <RatingStar key={x + i} addClass={x}/>    
+            )}            
         </div>            
     </div>
 })
