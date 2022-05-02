f19c3469e3005a251bcee498a6a7770f913998ff
dimasikBurdin <81648493+dimasikBurdin@users.noreply.github.com>
2022-05-02 16:20:22 +0500

    небольшой фикс модалки, попытки сделать норм верстку с волнами

--- frontend-react-web/src/components/pages/StartPage/components/footerStartPage/footerStartPage.tsx
+++ frontend-react-web/src/components/pages/StartPage/components/footerStartPage/footerStartPage.tsx
@@ -3,7 +3,7 @@ import './footerStartPage.css';
 
 export const FooterStartPage:React.FC = React.memo(() => {
     return <div className="start-page-footer-container">
-        <div className="start-page-footer-wave-first"></div>
-        <div className="start-page-footer-wave-second"></div>
+        <div className="start-page-footer-wave-first" onMouseMove={() => console.log('левая')}></div>
+        <div className="start-page-footer-wave-second" onMouseMove={() => console.log('правая')}></div>
     </div>
 })
