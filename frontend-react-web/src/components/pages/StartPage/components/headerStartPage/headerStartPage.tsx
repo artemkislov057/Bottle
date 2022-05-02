99bc45d6d6dab094f955bee8d88b7cad78fcba55
dimasikBurdin <81648493+dimasikBurdin@users.noreply.github.com>
2022-05-02 18:02:51 +0500

    сделана нормальная адаптивная(+-) верстка волн на стартовой стр, добавлена анимация на кнопку Begin

--- frontend-react-web/src/components/pages/StartPage/components/headerStartPage/headerStartPage.tsx
+++ frontend-react-web/src/components/pages/StartPage/components/headerStartPage/headerStartPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './headerStartPage.css';
-import ic from './headerWave.svg';
+import ic from './headerWaveNew1.svg';
+// import ic from './fuuuuuuuuck.png';
 
 type TProps = {
     onClickSignUp: Function
@@ -8,7 +9,11 @@ type TProps = {
 
 export const HeaderStartPage:React.FC<TProps> = React.memo((props) => {
     return <div className="start-page-header-container">
-        <div className="start-page-header-wave-first" ></div>
+        {/* <div className="start-page-header-wave-first-container"> */}
+            <div className="start-page-header-wave-first" ></div>
+            {/* <img className="start-page-header-wave-first" src={ic} ></img> */}
+        {/* </div> */}
+        
         <div className="start-page-header-wave-second"></div>
         <div className="start-page-header-sign-container">
             <button className="start-page-header-exit-button">
