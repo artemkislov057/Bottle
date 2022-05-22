03476d349091ae36062967b2df1b4009a04ea59c
Nikita4513 <predein697@gmail.com>
2022-05-22 17:02:16 +0500

    fix login-form

--- admin-angular-app/src/app/modules/login/components/login-form/login-form.component.ts
+++ admin-angular-app/src/app/modules/login/components/login-form/login-form.component.ts
@@ -13,8 +13,8 @@ export class LoginFormComponent implements OnInit {
   private readonly emailRegex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 
   public loginForm: FormGroup = new FormGroup({
-    email: new FormControl('', Validators.pattern(this.emailRegex)),
-    password: new FormControl('', Validators.minLength(6))
+    email: new FormControl('', [Validators.pattern(this.emailRegex), Validators.required]),
+    password: new FormControl('', [Validators.minLength(6), Validators.required])
   });
 
   constructor(
