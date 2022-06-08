c7ba0da5d0b248b08d1bcab742cc74faa51ba3b5
dimasikBurdin <81648493+dimasikBurdin@users.noreply.github.com>
2022-06-08 14:37:01 +0500

    доверстана #2 форма коммерц регистрации, прописана логика регистрации

--- frontend-react-web/src/components/pages/StartPage/components/commercRegistrPage/dataContainer.tsx
+++ frontend-react-web/src/components/pages/StartPage/components/commercRegistrPage/dataContainer.tsx
@@ -2,22 +2,33 @@ import React from "react";
 
 type TProps = {
     headerName: string,
-    fieldsNames: Array<string>
+    fieldsNames: Array<{field: string, title: string, type?: string}>
+    updateData: Function
 }
 
 export const DataContainer:React.FC<TProps> = React.memo((props) => {
     return <div className="commerc-registration-data-container">
-        <div className="commerc-registration-data-container-header">
-            {props.headerName}
-        </div>
+        {
+            props.headerName ?
+                <div className="commerc-registration-data-container-header">
+                    {props.headerName}
+                </div>
+                : null
+        }        
         <div className="commerc-registration-data-container-data-fields">
-            {props.fieldsNames.map(name => {
-                return <div key={name} className="commerc-registration-data-container-input-field">
-                    <label className="commerc-registration-data-container-input-field-label" htmlFor={`commerc-registration-data-container-input-field-input ${name}`}>{name}</label>
+            {props.fieldsNames.map(data => {
+                return <div key={data.field} className="commerc-registration-data-container-input-field">
+                    <label 
+                        className="commerc-registration-data-container-input-field-label" 
+                        htmlFor={`commerc-registration-data-container-input-field-input ${data.field}`}>
+                            {data.title}
+                    </label>
                     <input 
-                        className={`commerc-registration-data-container-input-field-input ${name}`}
-                        id={`commerc-registration-data-container-input-field-input ${name}`}
+                        className={`commerc-registration-data-container-input-field-input ${data.field}`}
+                        id={`commerc-registration-data-container-input-field-input ${data.field}`}
+                        onChange={e => props.updateData(data.field, e.target.value)}
                         required
+                        type={data.type ? data.type : 'text'}
                     >
                     </input>
                 </div>                
