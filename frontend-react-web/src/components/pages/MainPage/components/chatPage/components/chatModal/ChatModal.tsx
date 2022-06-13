ecace840187b4ca725534ef78b8afa50b76e5550
dimasikBurdin <81648493+dimasikBurdin@users.noreply.github.com>
2022-06-13 16:13:38 +0500

    доделана модалка оценки пользователя, отрисовка звезд немного некорректна

--- frontend-react-web/src/components/pages/MainPage/components/chatPage/components/chatModal/ChatModal.tsx
+++ frontend-react-web/src/components/pages/MainPage/components/chatPage/components/chatModal/ChatModal.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './ChatModal.css';
+import rateIcon from './ratePartnerCloseDialogIcon.svg';
 
 type TProps = {
     id: number,
@@ -12,10 +13,12 @@ type TProps = {
 
 export const ChatModal:React.FC<TProps> = React.memo((props) => {
     function onClickNumber(value: number){
-        props.setRate({id: props.id, value: value});
+        // props.setRate({id: props.id, value: value});
+        console.log(value)
     }
     return <div className="chat-modal-container">
         <div className="chat-modal-window">
+            <img className="chat-modal-window-icon" alt="смайлики" src={rateIcon} />
             <div className="chat-modal-window-text">
                 <span className="chat-modal-window-title">
                     Пожалуйста, оцените вашего собеседника <span className="chat-modal-window-title-name">{props.partnerName}</span>
@@ -23,11 +26,11 @@ export const ChatModal:React.FC<TProps> = React.memo((props) => {
                 <span className="chat-modal-window-caption">Ваш отзыв о собеседнике помогает нам развиваться в нужном направлении</span>
             </div>            
             <div className="chat-modal-window-rate-buttons">
-                <button className="chate-modal-rate-button" onClick={() => onClickNumber(1)}>1</button>
-                <button className="chate-modal-rate-button" onClick={() => onClickNumber(2)}>2</button>
-                <button className="chate-modal-rate-button" onClick={() => onClickNumber(3)}>3</button>
-                <button className="chate-modal-rate-button" onClick={() => onClickNumber(4)}>4</button>
-                <button className="chate-modal-rate-button" onClick={() => onClickNumber(5)}>5</button>
+                <button className="chat-modal-rate-button" onClick={() => onClickNumber(1)}></button>
+                <button className="chat-modal-rate-button" onClick={() => onClickNumber(2)}></button>
+                <button className="chat-modal-rate-button" onClick={() => onClickNumber(3)}></button>
+                <button className="chat-modal-rate-button" onClick={() => onClickNumber(4)}></button>
+                <button className="chat-modal-rate-button" onClick={() => onClickNumber(5)}></button>
             </div>            
         </div>        
     </div>
